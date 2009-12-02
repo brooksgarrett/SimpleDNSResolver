d7ee3f9c19fcc58d03d30c2b644652ce5920712b
unknown <Brooks@.(none)>
2009-12-01 20:50:13 -0500

    Initial Commit, basic functionality.
    No error handling or validation.
    
    Signed-off-by: unknown <Brooks@.(none)>

---
+++ SimpleDNS/MainWindow.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace SimpleDNS
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.browseBTN = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.goBTN = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // browseBTN
+            // 
+            this.browseBTN.Location = new System.Drawing.Point(403, 9);
+            this.browseBTN.Name = "browseBTN";
+            this.browseBTN.Size = new System.Drawing.Size(75, 23);
+            this.browseBTN.TabIndex = 1;
+            this.browseBTN.Text = "Browse . . .";
+            this.browseBTN.UseVisualStyleBackColor = true;
+            this.browseBTN.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // goBTN
+            // 
+            this.goBTN.Location = new System.Drawing.Point(13, 40);
+            this.goBTN.Name = "goBTN";
+            this.goBTN.Size = new System.Drawing.Size(75, 23);
+            this.goBTN.TabIndex = 2;
+            this.goBTN.Text = "Resolve";
+            this.goBTN.UseVisualStyleBackColor = true;
+            this.goBTN.Click += new System.EventHandler(this.doResolve);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 69);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(565, 286);
+            this.textBox2.TabIndex = 3;
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 367);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.goBTN);
+            this.Controls.Add(this.browseBTN);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainWindow";
+            this.Text = "MainWindow";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button browseBTN;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button goBTN;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
---
+++ SimpleDNS/MainWindow.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Net;
+
+namespace SimpleDNS
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void doResolve(object sender, EventArgs e)
+        {
+            StreamReader input = new StreamReader(openFileDialog1.OpenFile());
+
+            string line = String.Empty;
+
+            while ((line = input.ReadLine()) != null)
+            {
+
+                IPHostEntry MyHost = Dns.Resolve(line);
+
+                textBox2.Text += line + "\r\n";
+                foreach (IPAddress MyIP in MyHost.AddressList)
+                {
+                    textBox2.Text += ("\t" + MyIP.ToString() + "\r\n");
+                }
+            }
+        }
+    }
+}
